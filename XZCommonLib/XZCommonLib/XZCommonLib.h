009c48054e79458b0e687fa5a890f40394e7b314
邢雅杰 <470446087@qq.com>
2022-11-04 21:50:12 +0800

    XZCommonLib

---
+++ XZCommonLib/XZCommonLib/XZCommonLib.h
@@ -0,0 +1,12 @@
+//
+//  XZCommonLib.h
+//  XZCommonLib
+//
+//  Created by LuckyXYJ on 2022/11/3.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XZCommonLib : NSObject
+
+@end
---
+++ XZCommonLib/XZCommonLib/XZCommonLib.m
@@ -0,0 +1,12 @@
+//
+//  XZCommonLib.m
+//  XZCommonLib
+//
+//  Created by LuckyXYJ on 2023/11/3.
+//
+
+#import "XZCommonLib.h"
+
+@implementation XZCommonLib
+
+@end

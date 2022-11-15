52af6e51ebb8dfd04f8226210f1dda930c209008
邢雅杰 <470446087@qq.com>
2022-11-15 21:50:12 +0800

    视图控制器工具

--- XZCommonLib/XZCommonLib/UIUtils.h
+++ XZCommonLib/XZCommonLib/UIUtils.h
@@ -6,10 +6,35 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface UIUtils : NSObject
+
+/**
+ *  获取当前正被展示的页面控制器
+ *
+ *  @return 控制器对象
+ */
+extern UIViewController * _Nullable CurrentDisplayController(void);
+
+/**
+ *  获取当前应用被展示的Window
+ *
+ *  @return Window对象
+ */
+extern UIWindow * _Nullable CurrentAppWindow(void);
+
+/**
+ *  获取某页面控制器栈顶的控制器对象
+ *
+ *  @param viewController 目标查询控制器对象
+ *
+ *  @return 栈顶控制器对象
+ */
+extern UIViewController * _Nonnull GetTopViewController(UIViewController * _Nonnull viewController);
+
 
 @end
 

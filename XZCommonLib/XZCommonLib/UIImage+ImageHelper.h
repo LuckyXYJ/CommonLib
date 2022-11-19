984db856c1021a51936924a019c624ab5d005600
邢雅杰 <470446087@qq.com>
2022-11-19 21:50:12 +0800

    iamge分类

---
+++ XZCommonLib/XZCommonLib/UIImage+ImageHelper.h
@@ -0,0 +1,16 @@
+//
+//  UIImage+ImageHelper.h
+//  XZCommonLib
+//
+//  Created by LuckyXYJ on 2023/3/12.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIImage (ImageHelper)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ XZCommonLib/XZCommonLib/UIImage+ImageHelper.m
@@ -0,0 +1,12 @@
+//
+//  UIImage+ImageHelper.m
+//  XZCommonLib
+//
+//  Created by LuckyXYJ on 2023/3/12.
+//
+
+#import "UIImage+ImageHelper.h"
+
+@implementation UIImage (ImageHelper)
+
+@end

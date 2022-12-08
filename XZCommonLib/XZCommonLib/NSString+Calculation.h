0b81798e3680ca5ce3e3ba6ba50999a1500e6f23
邢雅杰 <470446087@qq.com>
2022-12-08 20:51:12 +0800

    字符串 Calculation

---
+++ XZCommonLib/XZCommonLib/NSString+Calculation.h
@@ -0,0 +1,16 @@
+//
+//  NSString+Calculation.h
+//  XZCommonLib
+//
+//  Created by LuckyXYJ on 2023/3/12.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSString (Calculation)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ XZCommonLib/XZCommonLib/NSString+Calculation.m
@@ -0,0 +1,35 @@
+//
+//  NSString+Calculation.m
+//  XZCommonLib
+//
+//  Created by LuckyXYJ on 2023/3/12.
+//
+
+#import "NSString+Calculation.h"
+
+@implementation NSString (Calculation)
+
+- (CGRect)calculateLabelRectWithSize:(CGSize)size font:(UIFont *)font;
+- (CGRect)calculateLabelRectWithWidth:(float)width fontsize:(float)fontsize;
+- (CGRect)calculateLabelRectWithHeight:(float)height fontSize:(float)fontsize;
+- (CGFloat)calculateLabelWidthWithHeight:(float)height fontSize:(float)fontsize;
+- (CGFloat)calculateLabelHeightWithWidth:(float)width fontsize:(float)fontsize;
+- (CGFloat)calculateLabelHeightWithWidth:(float)width font:(UIFont*)font;
+
+/**
+ *  返回字符串所占用的尺寸
+ *
+ *  @param font    字体
+ *  @param maxSize 最大尺寸
+ */
+- (CGSize)sizeWithFont:(UIFont *)font maxSize:(CGSize)maxSize;
+
+//计算NSString中英文字符串的字符长度。ios 中一个汉字算2字符数
+- (int)charCount;
+
+//小写转大写
+- (NSString *)stringToUpper:(NSString *)str;
+//判断是否是字母
+- (BOOL)stringLetter:(NSString *)str;
+
+@end

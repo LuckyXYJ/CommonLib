b492f100bf8b508b229b9c6ec080dd9b6985b149
邢雅杰 <470446087@qq.com>
2022-12-11 20:51:12 +0800

    字符串 Calculation

--- XZCommonLib/XZCommonLib/NSString+Calculation.m
+++ XZCommonLib/XZCommonLib/NSString+Calculation.m
@@ -6,30 +6,80 @@
 //
 
 #import "NSString+Calculation.h"
+#import "CommonFont.h"
+#import "Dimens.h"
 
 @implementation NSString (Calculation)
 
-- (CGRect)calculateLabelRectWithSize:(CGSize)size font:(UIFont *)font;
-- (CGRect)calculateLabelRectWithWidth:(float)width fontsize:(float)fontsize;
-- (CGRect)calculateLabelRectWithHeight:(float)height fontSize:(float)fontsize;
-- (CGFloat)calculateLabelWidthWithHeight:(float)height fontSize:(float)fontsize;
-- (CGFloat)calculateLabelHeightWithWidth:(float)width fontsize:(float)fontsize;
-- (CGFloat)calculateLabelHeightWithWidth:(float)width font:(UIFont*)font;
+- (CGRect)calculateLabelRectWithWidth:(float)width fontsize:(float)fontsize {
+    return [self calculateLabelRectWithSize:CGSizeMake(width, MAXFLOAT) font:Font(fontsize)];
+}
 
-/**
- *  返回字符串所占用的尺寸
- *
- *  @param font    字体
- *  @param maxSize 最大尺寸
- */
-- (CGSize)sizeWithFont:(UIFont *)font maxSize:(CGSize)maxSize;
+- (CGRect)calculateLabelRectWithSize:(CGSize)size font:(UIFont *)font {
+    NSDictionary *attributes = @{NSFontAttributeName : font};
+    NSInteger options = NSStringDrawingUsesFontLeading | NSStringDrawingTruncatesLastVisibleLine | NSStringDrawingUsesLineFragmentOrigin;
+    return [self boundingRectWithSize:size options:(NSStringDrawingOptions)options attributes:attributes context:nil];
+}
 
-//计算NSString中英文字符串的字符长度。ios 中一个汉字算2字符数
-- (int)charCount;
+- (CGRect)calculateLabelRectWithHeight:(float)height fontSize:(float)fontsize {
+    return [self calculateLabelRectWithSize:CGSizeMake(MAXFLOAT, height) font:Font(fontsize)];
+}
+
+- (CGFloat)calculateLabelWidthWithHeight:(float)height fontSize:(float)fontsize {
+    CGRect rect = [self calculateLabelRectWithSize:CGSizeMake(MAXFLOAT, height) font:Font(fontsize)];
+    return ceil(rect.size.width);
+}
+
+- (CGFloat)calculateLabelHeightWithWidth:(float)width fontsize:(float)fontsize {
+    CGRect rect = [self calculateLabelRectWithSize:CGSizeMake(width, MAXFLOAT) font:Font(fontsize)];
+    return ceil(rect.size.height);
+}
+- (CGFloat)calculateLabelHeightWithWidth:(float)width font:(UIFont*)font {
+    CGRect rect = [self calculateLabelRectWithSize:CGSizeMake(width, MAXFLOAT) font:font];
+    return ceil(rect.size.height);
+}
+
+- (CGSize)sizeWithFont:(UIFont *)font maxSize:(CGSize)maxSize
+{
+    NSDictionary *attrs = @{NSFontAttributeName : font};
+    return [self boundingRectWithSize:maxSize options:NSStringDrawingUsesLineFragmentOrigin attributes:attrs context:nil].size;
+}
+
+- (int)charCount {
+    int strlength = 0;
+    char* p = (char*)[self cStringUsingEncoding:NSUnicodeStringEncoding];
+    for (int i = 0; i < [self lengthOfBytesUsingEncoding:NSUnicodeStringEncoding]; i++) {
+        if (*p) {
+            p++;
+            strlength++;
+        } else {
+            p++;
+        }
+    }
+    return strlength;
+}
 
 //小写转大写
-- (NSString *)stringToUpper:(NSString *)str;
+- (NSString *)stringToUpper:(NSString *)str {
+    for (NSInteger i = 0; i < str.length; i++) {
+        if ([str characterAtIndex:i] >= 'a' & [str characterAtIndex:i] <= 'z') {
+            char temp = [str characterAtIndex:i] - 32;
+            NSRange range = NSMakeRange(i, 1);
+            str = [str stringByReplacingCharactersInRange:range withString:[NSString stringWithFormat:@"%c",temp]];
+        }
+    }
+    return str;
+}
+
 //判断是否是字母
-- (BOOL)stringLetter:(NSString *)str;
+- (BOOL)stringLetter:(NSString *)str {
+    NSString *regex = @"[A-Za-z]+";
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"SELF MATCHES %@",regex];
+    if ([predicate evaluateWithObject:str]) {
+        return YES;
+    }else {
+        return NO;
+    }
+}
 
 @end

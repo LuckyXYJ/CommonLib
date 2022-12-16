b78cd160186a91531f7b38be71a92d6a5ab3bb87
邢雅杰 <470446087@qq.com>
2022-12-16 21:51:12 +0800

    时间类扩展

---
+++ XZCommonLib/XZCommonLib/NSDate+Extension.h
@@ -0,0 +1,16 @@
+//
+//  NSDate+Extension.h
+//  XZCommonLib
+//
+//  Created by LuckyXYJ on 2023/3/12.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSDate (Extension)
+
+@end
+
+NS_ASSUME_NONNULL_END
